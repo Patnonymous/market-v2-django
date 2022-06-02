8aa7a63fde7430e3e33b38c3bf7341b8a8155013
Patrick_Williamson <patrickw9080@outlook.com>
2022-06-02 11:42:39 -0400

    Added comments.

--- market_project/marketplace/views.py
+++ market_project/marketplace/views.py
@@ -1,20 +1,34 @@
+# Imports Django.
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 
+# Imports custom.
 from .forms import CustomUserCreationForm
 from .models import MarketItem
 
 
 # Create your views here.
 class SignUpView(CreateView):
+    """
+    SignUpView contains form for new account creation.
+    Extends CreateView and UserCreationForm so supports error messages and redirect
+    out of the box.
+
+    Rather ugly right now.
+    """
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
 
 class IndexView(ListView):
+    """
+    IndexView extends ListView and will display Featured market items.
+
+    The class may have to be extended and edited later.
+    """
     template_name = 'marketplace/base_index.html'
     context_object_name = 'featured_item_listings'
 
